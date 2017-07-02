9ae3a3df1f33f3c168d56b6779dc2f0c264abb6a
shell <shell@LAPTOP-258RIN60>
2017-07-02 02:39:58 -0300

    fim Q1.

--- P2EXE1SHELLY/src/main/java/ita/P2EXE1SHELLY/Comprador.java
+++ P2EXE1SHELLY/src/main/java/ita/P2EXE1SHELLY/Comprador.java
@@ -3,7 +3,7 @@ package ita.P2EXE1SHELLY;
 public interface Comprador {
 	
 	
-	void addBookToLaterSells(Livraria book);
+	
 	enum situaçãodevenda {NONE, SENT, ARRIVED};
 	situaçãodevenda checkSellingStatus(String bname);
 
