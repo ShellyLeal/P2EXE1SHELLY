d97a28877753a92b0b43e15f387aac9a322aa0f7
ShellyLeal <shellyleal.ita@gmail.com>
2017-07-02 14:31:50 -0300

    Update Comprador.java

--- P2EXE1SHELLY/src/main/java/ita/P2EXE1SHELLY/Comprador.java
+++ P2EXE1SHELLY/src/main/java/ita/P2EXE1SHELLY/Comprador.java
@@ -1,10 +1,7 @@
 package ita.P2EXE1SHELLY;
 
 public interface Comprador {
-	
-	
-	
-	enum situaçãodevenda {NONE, SENT, ARRIVED};
-	situaçãodevenda checkSellingStatus(String bname);
+	enum situaçãodevenda {NONE, SENT, ARRIVED}; // basic enum for book situation for purchaser
+	situaçãodevenda checkSellingStatus(String livro);
 
 }
